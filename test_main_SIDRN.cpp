0be58355d02adcee88d4496f4e19f060bcce7f04
zeplaz <life.in.the.vivid.dream@gmail.com>
2019-01-12 20:42:42 -0500

    updatecomite_jan_11vr.0.0.0012

--- test_main_SIDRN.cpp
+++ test_main_SIDRN.cpp
@@ -7,7 +7,9 @@
 
 #include <cstdio>
 #include <fstream>
+#include <iostream>
 #include <ctime>
+#include <limits>
 //libs thirdparty
 
 
@@ -16,8 +18,12 @@
 //macos and debug stuff
 #define DEBUGZ
 
+//gloabal-staticz
 
 
+//gobal varbz
+
+bool quit = false;
 
 
 int main(int argc, char* argv[])
@@ -28,9 +34,9 @@ std::fstream log_file_stream;
 time_t now = time(0);
 tm* localtm = localtime(&now);
 
-printf(" SUDRN_0.00.01 Main Execution \n" );
-log_file_stream.open("log_file.txt", std::fstream::in | std::fstream::out | std::fstream::app);
-log_file_stream << "SIDRN begain exuection: " << asctime(localtm) << "\n";
+  printf(" SUDRN_0.00.01 Main Execution \n" );
+  log_file_stream.open("log_file.txt", std::fstream::in | std::fstream::out | std::fstream::app);
+  log_file_stream << "SIDRN begain exuection: " << asctime(localtm) << "\n";
 
 //inlization proccedures
 
@@ -39,6 +45,29 @@ log_file_stream << "SIDRN begain exuection: " << asctime(localtm) << "\n";
 
 
 
+
+
+ printf(" To Quit enter q and press enter:\n");
+//mainloop
+
+
+  while(!quit)
+  {
+    //local varz
+    char userinput;
+
+
+//grab a char from concelz
+    std::cin >> userinput;
+
+    if (userinput == 'q')
+      {
+        quit = true;
+      }
+//wipes the stream if not vaild
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
 
 
 
